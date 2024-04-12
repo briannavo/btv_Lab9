2a886897196313ca8a5939381b04584125b68021
Brianna Vo <briannavo2016@gmail.com>
2024-04-12 13:09:13 -0400

    third commit

--- btv_Lab9.py
+++ btv_Lab9.py
@@ -1,4 +1,4 @@
-# Raul Rojas
+# Brianna Vo
 def print_menu():
     print("Menu")
     print("-------------")
@@ -14,22 +14,33 @@ def encode_password(password):
         num += 3
         new_password += str(num)
 
-    return int(new_password)
+    return new_password
 
 
-while True:
+# code from Raul Rojas
+def decode_password(encoded_password):
+    original_password = ''
+    encoded_password = str(encoded_password)
+    for num in encoded_password:
+        num = int(num)
+        num -= 3
+        original_password += str(num)  # Ensure digits wrap around below 0
+
+    return original_password
+
+def main():
     print_menu()
-    option = input("Please enter an option: ")
-    encoded_password = 0
-    original_password = 0
-    if option == "1":
-        password_to_encode = int(input("Please enter your password to encode: "))
-        original_password = password_to_encode
-        encoded_password = encode_password(password_to_encode)
-        print("Your password has been encoded and stored!")
+    while True:
+        option = input("Please enter an option: ")
+        if option == "1":
+            password_to_encode = int(input("Please enter your password to encode: "))
+            original_password = password_to_encode
+            encoded_password = encode_password(password_to_encode)
+            print("Your password has been encoded and stored!")
+        if option == "2":
+            decoded_password = decode_password(encoded_password)
+            print(f"The encoded password is: {encoded_password}, and the original password is {decoded_password}")
+        if option == '3':
+            break
 
-        print(f"{encoded_password}")
-
-    if option == '3':
-        break
-
+main()
